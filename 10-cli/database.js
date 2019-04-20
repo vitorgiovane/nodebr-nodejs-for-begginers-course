eedcd3d517ea3f0121c29cf61edc51d72075fb4e
Vitor <vitorgpalves@gmail.com>
2019-04-20 19:08:13 -0300

    Finish class Handling files - CREATE

--- 10-cli/database.js
+++ 10-cli/database.js
@@ -1,6 +1,7 @@
-const { readFile } = require("fs")
+const { readFile, writeFile } = require("fs")
 const { promisify } = require("util")
 const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 
 class Database {
   constructor() {
@@ -11,7 +12,25 @@ class Database {
     return JSON.parse(file.toString())
   }
 
-  writeFile() {}
+  async writeFile(data) {
+    await writeFileAsync(this.FILE_NAME, JSON.stringify(data))
+    return true
+  }
+
+  async register(hero) {
+    const data = await this.getFileData()
+    const id = hero.id <= 2 ? hero.id : Date.now()
+    const heroWithId = {
+      id,
+      ...hero
+    }
+    const finalData = [
+      ...data,
+      heroWithId
+    ]
+    const result = await this.writeFile(finalData)
+    return result
+  }
 
   async list(id) {
     const data = await this.getFileData()
--- 10-cli/test.js
+++ 10-cli/test.js
@@ -3,9 +3,18 @@ const database = require("./database")
 const ITEM_DEFAULT = { name: "Flash", power: "Speed", id: 1 }
 
 describe("Heroes manipulation suite", () => {
+  before(async () => {
+    await database.register(ITEM_DEFAULT)
+  })
   it("Must search a hero using files", async () => {
     const expected = ITEM_DEFAULT
     const [ result ] = await database.list(expected.id)
     deepEqual(result, expected)
+  })
+  it("Must register a hero using files", async () => {
+    const expected = ITEM_DEFAULT
+    const result =  await database.register(ITEM_DEFAULT)
+    const [ hero ] = await database.list(ITEM_DEFAULT.id)
+    deepEqual(hero, expected)
   })
 })
